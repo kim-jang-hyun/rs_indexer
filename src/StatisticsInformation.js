6c5566ed4fe8368217f52757ff35ac433daf307b
kim-jang-hyun <kchdagitul@hanmail.net>
2024-01-01 11:36:22 +0900

    Add files via upload

--- src/StatisticsInformation.js
+++ src/StatisticsInformation.js
@@ -8,9 +8,7 @@ export default function StatisticsInformation({
   statistics_information1,
   chart_name2,
   statistics_information2,
-  statistics_information3,
-  statistics_information4,
-  statistics_information5
+  statistics_information3
 }) {
   return (
     <React.Fragment>
@@ -19,19 +17,9 @@ export default function StatisticsInformation({
         {statistics_information1}
       </Typography>
       <br />
-      <Title>{chart_name1} 경향성</Title>
-      <Typography variant="body2" color="text.secondary" sx={{ flex: 1 }}>
-        {statistics_information4}
-      </Typography>
-      <br />
       <Title>{chart_name2} 통계 안내</Title>
       <Typography variant="body2" color="text.secondary" sx={{ flex: 1 }}>
         {statistics_information2}
-      </Typography>
-      <br />
-      <Title>{chart_name2} 경향성</Title>
-      <Typography variant="body2" color="text.secondary" sx={{ flex: 1 }}>
-        {statistics_information5}
       </Typography>
       <br />
       <Title>
